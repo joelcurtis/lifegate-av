cb17147619c9c0757132a92af4165cbd499da39e
Joel Curtis <joel.caleb.curtis@gmail.com>
2021-06-19 19:00:05 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Lifegate AV System
+
+A simple web server running on Flask, that enables a Raspberry Pi to power on/off the Lifegate Padstow AV system.
+
+## Installation Instructions
+
+1. Log in to your Raspberry Pi (default username and password is `pi` and `raspberry`, respectively).
+2. Navigate to the root directory and install dependencies.
+
+	```
+	cd ~
+	sudo apt update
+	pip3 install flask
+	pip3 install RPi.GPIO
+	pip3 install paramiko
+	```
+
+3. Clone the repository.
+
+	```
+	git clone https://github.com/joelcurtis/lifegate-av.git
+	```
+
+4. To start the program on boot, edit `rc.local` by typing `sudo nano /etc/rc.local` and adding the below line just before `exit 0`.
+
+	```
+	sudo python /home/pi/lifegate-av/app.py &
+	```
+	
+	Save the file with 'CTRL + X' and press 'Enter'
---
+++ app.py
@@ -0,0 +1,108 @@
+from os import stat
+from flask import Flask, render_template
+import paramiko
+import RPi.GPIO as GPIO
+from time import sleep
+
+
+
+app = Flask(__name__)
+
+# SSH connection with Tesira Forte
+transport = None
+tesira = None
+
+def establishConnection():
+    global transport
+    global tesira
+    transport = paramiko.Transport(('192.168.1.49', 22))
+    transport.connect(username = 'default', password = 'default')
+    tesira = transport.open_channel('session')
+    tesira.get_pty()
+    tesira.invoke_shell()
+
+# Set GPIO pins to output
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+
+
+
+# --- HOME SCREEN ---
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+# --- POWER ---
+
+# Power On
+@app.route('/on')
+def on():
+    GPIO.output(11, GPIO.HIGH)
+    GPIO.output(13, GPIO.HIGH)
+    
+    establishConnection()
+    tesira.send('Level2 set mute 1 true'+'\n')
+    tesira.send('Level1 set mute 1 true'+'\n')
+    tesira.send('Level4 set mute 1 true'+'\n')
+    tesira.send('LGAmp1 set ampPower true'+'\n')
+    tesira.send('LGAmp2 set ampPower true'+'\n')
+
+    return render_template('index.html')
+
+# Power Off
+@app.route('/off')
+def off():
+    tesira.send('LGAmp1 set ampPower false'+'\n')
+    tesira.send('LGAmp2 set ampPower false'+'\n')
+    sleep(3)
+    transport.close()
+    
+    GPIO.output(11, GPIO.LOW)
+    GPIO.output(13, GPIO.LOW)
+
+    return render_template('index.html')
+
+
+
+# --- AUDIO ---
+
+# Audio Control
+@app.route('/audio')
+def audio():
+    tesira.send('Level2 set mute 1 false'+'\n')
+    tesira.send('Level1 set mute 1 false'+'\n')
+    tesira.send('Level4 set mute 1 false'+'\n')
+
+    return render_template('audio.html')
+
+# Level Up
+@app.route('/levelUp/<channel>')
+def levelUp(channel):
+    if channel == 'hh':
+        tesira.send('Level2 increment level 1 4'+'\n')
+    elif channel == 'pc':
+        tesira.send('Level1 increment level 1 4'+'\n')
+    elif channel == 'bt':
+        tesira.send('Level4 increment level 1 4'+'\n')
+    
+    return render_template('audio.html')
+
+# Level Down
+@app.route('/levelDown/<channel>')
+def levelDown(channel):
+    if channel == 'hh':
+        tesira.send('Level2 decrement level 1 4'+'\n')
+    elif channel == 'pc':
+        tesira.send('Level1 decrement level 1 4'+'\n')
+    elif channel == 'bt':
+        tesira.send('Level4 decrement level 1 4'+'\n')
+    
+    return render_template('audio.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
